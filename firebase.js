9deb741c05c2fe601fe1a363beaa5aa97a63df99
Jack Ryan <jack.dixon.ryan@gmail.com>
2018-07-10 19:41:12 -0600

    Testing geolocation updating

--- firebase.js
+++ firebase.js
@@ -1,3 +1,5 @@
+var pos;
+
 var config = {
     apiKey: "AIzaSyBJ1wWHUPZJQh97JtOr4UsmKMHSwWqG_kI",
     authDomain: "speedtrapp-5cd38.firebaseapp.com",
@@ -10,19 +12,24 @@ var config = {
 firebase.initializeApp(config);
 
 var database = firebase.database();
-
-navigator.geolocation.getCurrentPosition(function(position) {
-    var pos = {
+    
+navigator.geolocation.watchPosition(function(position) {
+    pos = {
         lat: position.coords.latitude,
         lng: position.coords.longitude
     };
-    $("#submit").on("click", function(){
-        database.ref().push({
-            
-            lat: pos.lat,
-            lng: pos.lng
-    
-        });
-    });
 });
 
+setInterval(function(){
+    console.log(pos);
+}, 1000);
+
+
+$("#submit").on("click", function(){
+    database.ref().push({
+        
+        lat: pos.lat,
+        lng: pos.lng
+
+    });
+});
